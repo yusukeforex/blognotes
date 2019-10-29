ad4bc1a5aa169d7740ce76c095e6c7620297a284
yusukeforex <momijina@icloud.com>
2019-10-29 12:01:14 +0900

    create user table and model

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_10_26_042349) do
+ActiveRecord::Schema.define(version: 2019_10_29_025347) do
 
   create_table "notes", force: :cascade do |t|
     t.string "title"
     t.text "description"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "name"
+    t.string "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
